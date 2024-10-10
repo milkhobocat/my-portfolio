d40b5f7f8df8f58c977b334ab002689825d889bd
milkhobocat <karrel.uy.2019@mumail.ie>
2024-10-10 22:24:39 +0100

    added 3d pin
    animated cards

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,5 +1,6 @@
 import { projects } from '@/data'
 import React from 'react'
+import { PinContainer } from './ui/3d-pin'
 
 const RecentProjects = () => {
   return (
@@ -12,7 +13,27 @@ const RecentProjects = () => {
         {projects.map(({id, title, des, img, iconLists, link}) => (
             <div key={id} className="lg:min-h-[32.5rem] h-[25rem] flex
              items-center justify-center sm:w-96 w-[80vw]">
-                {title}
+              <PinContainer title= {title} href={link}>
+                <div className='relative flex items-center justify-center 
+                sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10'>
+                  <div className="relative w-full h-full overflow-hidden lg:rounded-3xl
+                  bg-[#3C1A35]">
+                    <img src="/bg.png" alt="bg-img"/>
+                  </div>
+                  <img
+                    src={img}
+                    alt={title}
+                    className="z-10 absolute bottom-0"
+                  />
+                </div>
+                <h1 className="font-bold lg-text-2xl md:text-xl text-base line-clamp-1">
+                  {title}
+                </h1>
+                <p>
+                  {des}
+                </p>
+              </PinContainer>
+                
             </div>
         ))}
      </div>
